916366569a1bd2be92cfffb68e6126f1ae185a25
Anton Nazarenko <anton@code-care.pro>
2019-10-20 16:00:43 +0300

    load courses from array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import CourseCard from './components/Course'
+
+const courses = [<CourseCard 
+  price='12.99$'
+  rate='4.5'
+  name='C++ complete course.'
+  time='13h45m'
+  img='https://prod-discovery.edx-cdn.org/media/course/image/72abaeb3-0856-4e89-906a-8b6eef402a1f-6cea5a12d5cd.small.jpg'
+/>,
+<CourseCard 
+  price='9.99$'
+  rate='5.0'
+  name='Python complete course.'
+  time='3h30m'
+  img='https://i.ytimg.com/vi/lOVpSh_0pTQ/maxresdefault.jpg'
+/>,
+<CourseCard 
+  price='3.99$'
+  rate='3.5'
+  name='C# complete course.'
+  time='3h45m'
+  img='https://www.learningtree.com/images/course/grabbers/419-grabber.png'
+/>,
+  <CourseCard 
+  price='6.99$'
+  rate='4.5'
+  name='Unix complete course.'
+  time='3h45m'
+  img='https://i0.wp.com/www.apponwellness.com/wp-content/uploads/2018/05/Certificate-of-Unix-Shell-Scripting-Training-Course.png?fit=520%2C277&ssl=1'
+/>,
+<CourseCard 
+  price='11.99$'
+  rate='5.0'
+  name='React complete course.'
+  time='3h45m'
+  img='https://elysiumacademy.org/wp-content/uploads/2018/12/REACT-JS.jpg'
+/>,
+<CourseCard 
+  price='29.99$'
+  rate='5'
+  name='NODE.JS complete course.'
+  time='3h45m'
+  img='https://i.udemycdn.com/course/750x422/82778_94fa_6.jpg'
+/>]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Navbar />
+          <div className="course-container">
+           {courses} 
+          </div>
     </div>
   );
 }
---
+++ src/components/Course/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styles from './index.css'
+import { Icon } from 'react-fa'
+
+export default class CourseCard extends React.Component {
+
+    getStarRate (rate) {
+        let stars = []
+
+        const [int, decimal ] = rate.split('.')
+
+        for (let i = 1; i <= parseInt(int); i++) {
+            stars.push(<Icon name='star' />)
+        }
+        if (+decimal >= 5) {
+            stars.push(<Icon name='star-half-o' />)
+        }
+
+        for(let i = stars.length; i < 5; i++ ) {
+            stars.push(<Icon name='star-o' />)
+        }
+        
+        return (
+            <>
+            {stars} {rate}
+            </>
+        )
+    }
+
+    render () {
+      const { time, name, rate, img, price } = this.props
+
+      return ( 
+          <div className='card-container'
+          >
+              <div className="card-image">
+                  <img src={img} alt=""/>
+              </div>
+              <div className="card-footer">
+                   <div className="card-footer-top">
+                    <div className="card-name">
+                        {name}
+                    </div>
+                    <div className="card-rate">
+                            {this.getStarRate(rate)}
+                        </div>
+                    </div>
+                    <div className="card-footer-bot">
+                        <div className="card-time">
+                            <Icon name='clock-o' /> {time}
+                        </div>
+                        <div className="card-price">
+                            {price}
+                        </div>
+                    </div>
+              </div>
+          </div>
+      ) 
+    }
+}
